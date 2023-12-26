139396a48ca3627a34b9188ba730bf8380714d30
abdurrahmanamin <dfccxccc1@gmail.com>
2023-12-26 23:37:36 +0500

    assets made

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:zarafa/screens/auth/sign_up.dart';
 
 void main() {
   runApp(const MyApp());
@@ -25,7 +26,7 @@ class MyApp extends StatelessWidget {
             home: child,
           );
         },
-        child: const MyHomePage(title: 'Flutter Demo Home Page'));
+        child: SignUp());
   }
 }
 
